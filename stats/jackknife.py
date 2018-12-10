48e089835a8c8937498fcaf1dc9baa2022284054
Ty Garber <tygarber@gmail.com>
2018-12-09 21:19:46 -0800

    graphing...

--- stats/jackknife.py
+++ stats/jackknife.py
@@ -4,8 +4,15 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+import io
+import base64
+
+matplotlib.use('Agg')
+sns.set_style('darkgrid')
 
 def JackKnife(dataframe=None, predictor_column=None, result_column=None, year_column=None):
 
@@ -23,20 +30,26 @@ def JackKnife(dataframe=None, predictor_column=None, result_column=None, year_co
     if dataframe is None:
         raise ValueError('No dataframe')
     df = pd.read_json(dataframe)
+
+    df['Year'] = pd.to_datetime(df['Year'], format='%Y')
+    df.sort_values('Year', inplace=True)
+    print(df)
+
     predictor_mean = df[predictor_column].mean()
     result_mean = df[result_column].mean()
 
     lm = LinearRegression()
 
     df_lm = lm.fit(df[predictor_column].values.reshape(-1, 1),
-                   df[result_column].values.reshape(-1, 1) )
-    #coeeficient of entire dataframe
+                   df[result_column].values.reshape(-1, 1))
+
+    # coeeficient of entire dataframe
     coeff = df_lm.coef_[0][0]
 
-    #r2 of entire dataframe
+    # r2 of entire dataframe
     r2 = r2_score(df[result_column], y_pred=df_lm.predict(df[predictor_column].values.reshape(-1, 1)))
 
-    #empty columns to be filled
+    # empty columns to be filled
     df['b1'] = np.nan
     df['intercept'] = np.nan
     df['r2'] = np.nan
@@ -66,9 +79,26 @@ def JackKnife(dataframe=None, predictor_column=None, result_column=None, year_co
     mpe = df['pe'].mean() * 100
     mape = df['ape'].mean() * 100
 
-    results = {'mre': mre, 'mae': mae,
+    #plot this bad boy
+
+
+    img = io.BytesIO()
+    plot_df = df.set_index('Year')
+    fig, ax = plt.subplots()
+    ax.plot(plot_df[result_column], '--.', label='Observed')
+    ax.plot(plot_df['pred_y'], '--.', label='Estimated')
+    ax.set_ylabel(result_column)
+    ax.set_xlabel(year_column)
+    ax.legend()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    graph_url = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+
+    results = {'mre': mre, 'maSe': mae,
                'rmse': rmse, 'mpe': mpe,
-               'mape': mape, 'dataframe': df}
+               'mape': mape, 'dataframe': df.reset_index(drop=True),
+               'graph': 'data:image/png;base64,{}'.format(graph_url)}
 
     return results
 
